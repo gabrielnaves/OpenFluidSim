e889afa0ab13bc780c8b0461f609db667e4be70c
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-04-18 12:34:42 -0300

    Created a Simulation Grid static class.
    I'll be using it to ensure components are placed according to the grid.

---
+++ Assets/Scripts/Simulation/Grid Positioning/SimulationGrid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class SimulationGrid {
+
+    static public float cellSize = 0.4f;
+    
+    static public Vector2 FitToGrid(Vector2 point) {
+        return GridCoordinatesToWorldSpace(PointToGridCoordinates(point));
+    }
+
+    static public Vector2 PointToGridCoordinates(Vector2 point) {
+        return new Vector2(Mathf.Floor(point.x / cellSize), Mathf.Floor(point.y / cellSize));
+    }
+
+    static public Vector2 GridCoordinatesToWorldSpace(Vector2 gridPoint) {
+        return gridPoint * cellSize;
+    }
+}
