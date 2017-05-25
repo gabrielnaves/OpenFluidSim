abd7ef7054ffbe6563535fe9780799715c7dec95
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-05-25 04:20:47 -0300

    Started implmementing the "create component" functionality

---
+++ Assets/Scripts/Simulation/Component List/ComponentRef.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentRef : MonoBehaviour {
+
+    public GameObject floatingComponent;
+
+    public void SetFloatingComponent() {
+
+    }
+}
