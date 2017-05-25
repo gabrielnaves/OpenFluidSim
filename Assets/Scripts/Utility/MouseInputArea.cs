0ba6ef49789cd9d83689ccf159e3800f7460161d
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-05-25 05:57:05 -0300

    Created the MouseInputArea script
    This script is for making specific world areas where the mouse input is analyzed.

---
+++ Assets/Scripts/Utility/MouseInputArea.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class MouseInputArea : MonoBehaviour {
+
+    public bool mouseButton { get; private set; }
+    public bool mouseButtonDown { get; private set; }
+    public bool mouseButtonUp { get; private set; }
+    private Vector2 clickedPoint = Vector2.zero;
+
+    /// <summary>
+	/// Gets the click position. Returns Vector2.zero on frames with no input.
+	/// </summary>
+	public Vector2 GetClickPosition() {
+        return (mouseButton ? clickedPoint : Vector2.zero);
+    }
+    
+	void LateUpdate() {
+        bool lastMouseButton = mouseButton;
+        UpdateClickedPoint();
+        UpdateMouseButton();
+        UpdateMouseButtonDown(lastMouseButton);
+        UpdateMouseButtonUp(lastMouseButton);
+    }
+
+    private void UpdateClickedPoint() {
+        clickedPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void UpdateMouseButton() {
+        if (Input.GetMouseButtonDown(0))
+            mouseButton = GetComponent<BoxCollider2D>().OverlapPoint(clickedPoint);
+        else
+            mouseButton = false;
+    }
+
+    private void UpdateMouseButtonDown(bool lastMouseButton) {
+        if (mouseButton == true && lastMouseButton == false)
+            mouseButtonDown = true;
+        else
+            mouseButtonDown = false;
+    }
+
+    private void UpdateMouseButtonUp(bool lastMouseButton) {
+        if (mouseButton == false && lastMouseButton == true)
+            mouseButtonUp = true;
+        else
+            mouseButtonUp = false;
+    }
+}
