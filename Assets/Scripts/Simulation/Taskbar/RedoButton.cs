a9f72c1b9a105bf2640508503597d731368f5588
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-05-30 17:37:15 -0300

    Added keyboard shortcuts to undo/redo buttons
    Shortcutes are LShift+Z for undo and LShift+Y for redo, as using ctrl buttons result in unwanted interaction while in Unity Editor.

--- Assets/Scripts/Simulation/Taskbar/RedoButton.cs
+++ Assets/Scripts/Simulation/Taskbar/RedoButton.cs
@@ -3,20 +3,31 @@ using UnityEngine.UI;
 
 public class RedoButton : MonoBehaviour {
 
+    private bool active;
+
     void Update() {
         if (ActionStack.instance.redoStack.Count == 0)
             DeactivateButton();
         else
             ActivateButton();
+        if (active)
+            CheckForKeyboardShortcut();
     }
 
     private void DeactivateButton() {
         GetComponent<Image>().color = Color.gray;
         GetComponent<Button>().enabled = false;
+        active = false;
     }
 
     private void ActivateButton() {
         GetComponent<Image>().color = Color.white;
         GetComponent<Button>().enabled = true;
+        active = true;
+    }
+
+    private void CheckForKeyboardShortcut() {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Y))
+            GetComponent<Button>().onClick.Invoke();
     }
 }
--- Assets/Scripts/Simulation/Taskbar/UndoButton.cs
+++ Assets/Scripts/Simulation/Taskbar/UndoButton.cs
@@ -3,20 +3,31 @@ using UnityEngine.UI;
 
 public class UndoButton : MonoBehaviour {
 
-	void Update () {
+    private bool active;
+
+	void Update() {
         if (ActionStack.instance.actionStack.Count == 0)
             DeactivateButton();
         else
             ActivateButton();
+        if (active)
+            CheckForKeyboardShortcut();
     }
 
     private void DeactivateButton() {
         GetComponent<Image>().color = Color.gray;
         GetComponent<Button>().enabled = false;
+        active = false;
     }
 
     private void ActivateButton() {
         GetComponent<Image>().color = Color.white;
         GetComponent<Button>().enabled = true;
+        active = true;
+    }
+
+    private void CheckForKeyboardShortcut() {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Z))
+            GetComponent<Button>().onClick.Invoke();
     }
 }
