639ba14dbbd85d07f9fec6888c365eb204713df0
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-12 14:43:24 -0300

    All current components have pneumatic connectors.

---
+++ Assets/Scripts/Simulation/Component/ComponentConnectors.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentConnectors : MonoBehaviour {
+
+    public List<PneumaticConnector> connectorList { get; private set; }
+    public GameObject pneumaticConnectorPrefab;
+
+    private List<Vector3> connectorLocalPositions = new List<Vector3>();
+
+    void Awake() {
+        GetConnectorPositions();
+        RemoveDummyConnectors();
+        CreatePneumaticConnectors();
+    }
+
+    private void GetConnectorPositions() {
+        foreach (Transform child in transform)
+            connectorLocalPositions.Add(child.localPosition);
+    }
+
+    private void RemoveDummyConnectors() {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+    }
+
+    private void CreatePneumaticConnectors() {
+        connectorList = new List<PneumaticConnector>();
+        foreach (Vector3 localPositions in connectorLocalPositions) {
+            var newConnector = Instantiate(pneumaticConnectorPrefab);
+            newConnector.transform.parent = transform;
+            newConnector.transform.localPosition = localPositions;
+            connectorList.Add(newConnector.GetComponent<PneumaticConnector>());
+        }
+    }
+}
