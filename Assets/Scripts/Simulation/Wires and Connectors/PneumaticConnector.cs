a815ede15fef10441670180411c151c5848581bc
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-12 13:58:11 -0300

    Created stubs for connector and pressure line

---
+++ Assets/Scripts/Simulation/Wires and Connectors/PneumaticConnector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PneumaticConnector : MonoBehaviour {
+
+    public List<PneumaticConnector> connectedObjects = new List<PneumaticConnector>();
+
+    void Start() {
+        GetComponent<SpriteRenderer>().color = Color.magenta;
+    }
+
+    void Update() {
+
+    }
+}
