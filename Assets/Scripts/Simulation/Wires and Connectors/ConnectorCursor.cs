fa03ec7cdc3ad2273a5ae5538e4a727545f2dead
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-12 15:45:03 -0300

    The mouse cursor will change when hovering pneumatic connectors

---
+++ Assets/Scripts/Simulation/Wires and Connectors/ConnectorCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectorCursor : MonoBehaviour {
+
+    public Texture2D cursorTexture;
+    public Vector2 cursorHotspot = new Vector2(8, 8);
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    private Collider2D connectorCollider;
+    private bool isMouseHovering;
+
+    void Awake() {
+        connectorCollider = GetComponent<Collider2D>();
+    }
+
+    void Update() {
+        if (connectorCollider.OverlapPoint(SimulationInput.instance.GetMousePosition()) && !isMouseHovering) {
+            isMouseHovering = true;
+            Cursor.SetCursor(cursorTexture, cursorHotspot, cursorMode);
+        }
+        if (!connectorCollider.OverlapPoint(SimulationInput.instance.GetMousePosition()) && isMouseHovering) {
+            isMouseHovering = false;
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        }
+    }
+}
