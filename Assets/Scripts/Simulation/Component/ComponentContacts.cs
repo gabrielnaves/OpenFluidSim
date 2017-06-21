8a99cb44f19dd3748c001af336f315a26df76cc5
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-21 14:14:15 -0300

    Created mechanical contacts and added some to cylinder and sensor

---
+++ Assets/Scripts/Simulation/Component/ComponentContacts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentContacts : MonoBehaviour {
+
+    public List<GameObject> contactList { get; private set; }
+    public GameObject mechanicalContactPrefab;
+
+    private List<Vector3> contactLocalPositions = new List<Vector3>();
+
+    void Awake() {
+        GetContactPositions();
+        RemoveDummyContacts();
+        CreateMechanicalContacts();
+    }
+
+    private void GetContactPositions() {
+        foreach (Transform child in transform)
+            contactLocalPositions.Add(child.localPosition);
+    }
+
+    private void RemoveDummyContacts() {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+    }
+
+    private void CreateMechanicalContacts() {
+        contactList = new List<GameObject>();
+        foreach (Vector3 localPositions in contactLocalPositions) {
+            var newContact = Instantiate(mechanicalContactPrefab);
+            newContact.transform.parent = transform;
+            newContact.transform.localPosition = localPositions;
+            contactList.Add(newContact);
+        }
+    }
+}
