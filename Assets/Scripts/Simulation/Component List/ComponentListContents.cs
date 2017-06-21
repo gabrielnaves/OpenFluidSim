7a81fe3af08d0f2d0ac4ac6ddbf82ff5f2078327
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-21 11:11:10 -0300

    Component List will now scale its contents window with content amount.

---
+++ Assets/Scripts/Simulation/Component List/ComponentListContents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComponentListContents : MonoBehaviour {
+
+    private List<GameObject> guiComponents = new List<GameObject>();
+    private RectTransform rectTransform;
+
+    public void AddGUIComponent(GameObject component) {
+        guiComponents.Add(component);
+        component.GetComponent<RectTransform>().SetParent(rectTransform);
+        UpdateWidth();
+    }
+
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start() {
+        UpdateWidth();
+    }
+
+    void UpdateWidth() {
+        float width = 20;
+        foreach (RectTransform component in rectTransform)
+            width += 20 + component.rect.width;
+        rectTransform.sizeDelta = new Vector2(width, rectTransform.sizeDelta.y);
+    }
+}
