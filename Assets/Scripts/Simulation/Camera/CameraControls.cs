6c14e2690d1c1770c8a7b0b4ec400a725bf2856a
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-21 09:29:24 -0300

    Camera can now be moved around the simulation space

--- Assets/Scripts/Simulation/Camera/CameraControls.cs
+++ Assets/Scripts/Simulation/Camera/CameraControls.cs
@@ -5,8 +5,12 @@ public class CameraControls : MonoBehaviour {
     public float maxCameraSize = 7f;
     public float minCameraSize = 2f;
     public float scrollSpeed = 2f;
+    public float offsetScaling = 0.01f;
 
     private Camera mainCamera;
+    private bool movingCamera;
+    private Vector3 initialMousePos;
+    private Vector3 initialTransformPos;
 
     void Start() {
         mainCamera = GetComponent<Camera>();
@@ -14,11 +18,26 @@ public class CameraControls : MonoBehaviour {
 
 	void Update() {
         UpdateCameraZoom();
+        UpdateCameraPosition();
 	}
 
     private void UpdateCameraZoom() {
         float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
         mainCamera.orthographicSize = mainCamera.orthographicSize + scrollAxis * scrollSpeed;
         mainCamera.orthographicSize = Mathf.Clamp(mainCamera.orthographicSize, minCameraSize, maxCameraSize);
+    }
+
+    private void UpdateCameraPosition() {
+        if (Input.GetMouseButtonDown(2)/* && SimulationInput.instance.IsMouseOnInputArea()*/) {
+            movingCamera = true;
+            initialMousePos = Input.mousePosition;
+            initialTransformPos = transform.position;
+        }
+        else if (Input.GetMouseButtonUp(2))
+            movingCamera = false;
+        if (movingCamera) {
+            Vector3 offset = Input.mousePosition - initialMousePos;
+            transform.position = initialTransformPos - offset * offsetScaling;
+        }
     }
 }
