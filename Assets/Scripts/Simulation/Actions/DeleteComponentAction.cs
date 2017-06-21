bda992de59ae5faccf182e2c5b866f10f4032e8a
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-06-21 10:22:18 -0300

    DeleteComponentAction now manages pneumatic connections correctly.
    Pneumatic connections are removed when the component is deleted, and they're added again when the action is undone.

--- Assets/Scripts/Simulation/Actions/DeleteComponentAction.cs
+++ Assets/Scripts/Simulation/Actions/DeleteComponentAction.cs
@@ -5,11 +5,12 @@ public class DeleteComponentAction : IAction {
     public GameObject referencedObject;
 
     public void DoAction() {
+        DeactivateConnections(referencedObject.transform.GetChild(0).GetComponent<ComponentConnectors>());
         referencedObject.SetActive(false);
-        // Any other code relative to component deletion must go here
     }
 
     public void UndoAction() {
+        ReactivateConnections(referencedObject.transform.GetChild(0).GetComponent<ComponentConnectors>());
         referencedObject.SetActive(true);
     }
 
@@ -18,4 +19,24 @@ public class DeleteComponentAction : IAction {
     }
 
     public void OnDestroy() {}
+
+    private void DeactivateConnections(ComponentConnectors componentConnectors) {
+        foreach (var connector in componentConnectors.connectorList)
+            RemoveExternalConnections(connector);
+    }
+
+    private void RemoveExternalConnections(PneumaticConnector connector) {
+        foreach (var other in connector.connectedObjects)
+            other.RemoveConnection(connector);
+    }
+
+    private void ReactivateConnections(ComponentConnectors componentConnectors) {
+        foreach (var connector in componentConnectors.connectorList)
+            AddExternalConnections(connector);
+    }
+
+    private void AddExternalConnections(PneumaticConnector connector) {
+        foreach (var other in connector.connectedObjects)
+            other.AddConnection(connector);
+    }
 }
