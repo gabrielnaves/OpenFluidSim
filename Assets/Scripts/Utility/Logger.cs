b0ac25746940f678f835820f07671355066611b2
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-04-07 16:15:21 -0300

    Improving and documenting Logger class, documenting Singleton class.

--- Assets/Scripts/Utility/Logger.cs
+++ Assets/Scripts/Utility/Logger.cs
@@ -1,35 +1,68 @@
 ﻿using System.IO;
 using UnityEngine;
 
+/// <summary>
+/// MonoBehaviour for creating system text logs.
+/// </summary>
+/// The Logger class creates each log file using "program_log_" + local timestamp.
+/// Every log file is created inside "Logs/" folder.
+/// Log files are created if the System Logger object is in the current scene,
+/// but Log messages will not be stored if the Logger instance is disabled.
 public class Logger : MonoBehaviour {
 
     public string logFileName = "program_log_";
     public string logFileFolder = "Logs/";
 
+    static Logger instance;
+
     StreamWriter logFile;
 
     void Awake() {
+        InstanceSetup();
+        if (instance == this) {
+            GenerateLogFileNameWithTimestamp();
+            OpenLogFile();
+        }
+    }
+
+    void InstanceSetup() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    void GenerateLogFileNameWithTimestamp() {
         logFileName += System.DateTime.Now;
         logFileName = logFileName.Replace("/", "-");
         logFileName = logFileName.Replace(":", "-");
         logFileName = logFileName.Replace(" ", "_");
         logFileName += ".txt";
+    }
 
+    void OpenLogFile() {
         logFile = new StreamWriter(logFileFolder + logFileName, false, System.Text.Encoding.UTF8);
     }
 
     void OnDestroy() {
-        logFile.Close();
+        if (logFile != null)
+            logFile.Close();
     }
 
     void OnEnable() {
-        Application.logMessageReceived += HandleLog;
-        Debug.Log("Enabling logger");
+        if (instance == this) {
+            Application.logMessageReceived += HandleLog;
+            Debug.Log("Enabling logger");
+        }
     }
 
     void OnDisable() {
-        Debug.Log("Disabling logger");
-        Application.logMessageReceived -= HandleLog;
+        if (instance == this) {
+            Debug.Log("Disabling logger");
+            Application.logMessageReceived -= HandleLog;
+        }
     }
 
     void HandleLog(string logString, string stackTrace, LogType type) {
--- Assets/Scripts/Utility/Singleton.cs
+++ Assets/Scripts/Utility/Singleton.cs
@@ -1,11 +1,32 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Utility class for setting singletons
+/// </summary>
+/// The "singleton" referred to is created using automatic properties, and
+/// having the setter be private to the desired class.
+/// This implementation makes it easy to reference certain game objects through
+/// an attached component, when said object is intended to be unique on scene.
+/// 
+/// Note that this implementation by itself does not enforce the traditional
+/// singleton condition of "only one instance". You can still mess with it if
+/// you really feel like it.
+/// <example>
+/// public class Foo : MonoBehaviour {
+///     static public  Foo instance { get; private set; }
+///     
+///     void Awake() {
+///         instance = (Foo)Singleton.Setup(this, instance);
+///     }
+/// }
+/// </example>
 static public class Singleton {
-    public static MonoBehaviour Setup(MonoBehaviour source, MonoBehaviour destination) {
+
+    static public Object Setup(Object source, Object destination) {
         if (destination == null)
             destination = source;
         else
-            Debug.LogError("["+source+"Awake: Multiple instances of a singleton.");
+            Debug.LogError("["+source+"]Awake: Multiple instances of a singleton.");
         return destination;
     }
 }
