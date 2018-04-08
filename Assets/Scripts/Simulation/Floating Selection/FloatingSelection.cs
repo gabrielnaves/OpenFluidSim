9c934c1f5cccbe124379e241efc9b4ca5067f59a
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-04-08 20:07:36 -0300

    Documenting the floating selection

--- Assets/Scripts/Simulation/Floating Selection/FloatingSelection.cs
+++ Assets/Scripts/Simulation/Floating Selection/FloatingSelection.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// The place where the current floating component stays in
+/// until it is placed on editor canvas, or removed
+/// </summary>
+/// After the desired electric/pneumatic component is selected on the menu
+/// the corresponding object is instantiated as a floating component, and
+/// kept on the floating selection. When the component's position is
+/// chosen the actual component game object is instantiated and added to
+/// the simulation pane.
 public class FloatingSelection : MonoBehaviour {
 
     static public FloatingSelection instance { get; private set; }
 
-    private GameObject floatingComponent;
+    GameObject floatingComponent;
 
     public void AddComponent(GameObject component) {
         RemoveCurrentComponent();
