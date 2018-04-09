79989cdb0bbb03a1ba21948c63d19883ff6b1435
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-04-09 03:08:40 -0300

    Created a mainpage for the doxygen-generated documentation

---
+++ Assets/Scripts/mainpage.cs
@@ -0,0 +1,18 @@
+﻿/** \mainpage
+ * 
+ * This open-source pneumatic simulator is a work in progress being developed by me, Gabriel Naves, as 
+ * my final undergraduate assignment. It is intended to be used as a tool for teaching pneumatic systems
+ * in the [University of Brasília](http://www.unb.br/).
+ * 
+ * ## Check out the Latest version! 
+ * 
+ * The latest version of the simulator can always be found on [my website](http://gabrielnaves.com/simulator/index.html),
+ * but you can also download the latest version (for windows) from the project's
+ * [github repository](https://github.com/gabrielnaves/TG/releases).
+ * 
+ * ## Development
+ * 
+ * The simulator is being developed in [Unity](https://unity3d.com/). The game engine was chosen due to
+ * the possibility of building for [many different platforms](https://unity3d.com/unity/features/multiplatform).
+ * Unity allows scripting in C# and Javascript, and the language chosen for this project is C#. 
+ */
