b54ee6d2501b8bd36f72364b11527d7d8fb28b7d
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-04-13 12:53:48 -0300

    Adding the first version of the component library.
    It's a bad implementation, but will work for the time being. I'll eventually add a more automatic generation of the library. Also this library does not interact with the component list panel in any way.

---
+++ Assets/Scripts/Simulation/Component List/ComponentLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentLibrary : MonoBehaviour {
+
+    public string[] componentNames;
+    public GameObject[] componentPrefabs;
+
+    public Dictionary<string, GameObject> nameToPrefab { get; private set; }
+
+    void Awake() {
+        if (componentNames.Length != componentPrefabs.Length)
+            Debug.LogError("Component library configuration error");
+        else
+            BuildComponentTable();
+    }
+
+    void BuildComponentTable() {
+        nameToPrefab = new Dictionary<string, GameObject>();
+        for (int i = 0; i < componentNames.Length; ++i)
+            nameToPrefab[componentNames[i]] = componentPrefabs[i];
+    }
+}
