f4780e47ef24848739119ff009d33d5ef8cb1059
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-05-17 21:36:22 -0300

    Implemented a clipboard utility. Will be used when saving/loading on the web version.

---
+++ Assets/Scripts/Utility/Clipboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class Clipboard {
+
+    public static string GetClipboard() {
+        return GUIUtility.systemCopyBuffer;
+    }
+
+    public static void SetClipboard(string value) {
+        GUIUtility.systemCopyBuffer = value;
+    }
+
+    public static void EmptyClipboard() {
+        GUIUtility.systemCopyBuffer = "";
+    }
+}
