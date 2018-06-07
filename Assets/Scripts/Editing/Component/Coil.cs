1f754537c33f4106b71aeea71f2e38ff5d112183
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-06-06 23:58:36 -0300

    Implemented coil configuration for type (normal, on-delay and off-delay) and delay value.
    Save/Load utility will be updated on next commit, and then I'll implement simulation behaviour.

--- Assets/Scripts/Editing/Component/Coil.cs
+++ Assets/Scripts/Editing/Component/Coil.cs
@@ -1,11 +1,32 @@
-﻿public class Coil : CorrelationTarget {
+﻿using UnityEngine;
+
+public class Coil : CorrelationTarget, IConfigurable {
+
+    public enum Type { normal, on_delay, off_delay }
+
+    public GameObject coilConfigWindowPrefab;
+    public Sprite normalCoilSprite;
+    public Sprite onDelaySprite;
+    public Sprite offDelaySprite;
+    [ViewOnly] public Type type = Type.normal;
+    [ViewOnly] public float delay = 1f;
+
+    new Collider2D collider;
+    SpriteRenderer spriteRenderer;
+
+    void Awake() {
+        collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnEnable() {
         SimulationPanel.instance.AddCoil(this);
+        SimulationPanel.instance.AddConfigurable(this);
     }
 
     void OnDisable() {
         SimulationPanel.instance.RemoveCoil(this);
+        SimulationPanel.instance.RemoveConfigurable(this);
     }
 
     void LateUpdate() {
@@ -15,5 +36,38 @@
 
     public override string TypeString() {
         return "Coil";
+    }
+
+    bool IConfigurable.RequestedConfig() {
+        return EditorInput.instance.doubleClick &&
+            collider.OverlapPoint(EditorInput.instance.mousePosition);
+    }
+
+    void IConfigurable.OpenConfigWindow() {
+        var coilConfigWindow = Instantiate(coilConfigWindowPrefab).GetComponent<CoilConfigWindow>();
+        coilConfigWindow.coil = this;
+    }
+
+    bool IConfigurable.IsConfigured() {
+        return true;
+    }
+
+    public void UpdateType(Type type) {
+        this.type = type;
+        switch (type) {
+            case Type.normal:
+                spriteRenderer.sprite = normalCoilSprite;
+                break;
+            case Type.on_delay:
+                spriteRenderer.sprite = onDelaySprite;
+                break;
+            case Type.off_delay:
+                spriteRenderer.sprite = offDelaySprite;
+                break;
+        }
+    }
+
+    public void UpdateDelay(float delay) {
+        this.delay = delay;
     }
 }
---
+++ Assets/Scripts/GUI/Windows/CoilConfigWindow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoilConfigWindow : MonoBehaviour {
+
+    public Dropdown typeDropdown;
+    public InputField delayInputField;
+    public GameObject delayConfiguration;
+
+    [ViewOnly] public Coil coil;
+
+    public void SetCoilType(int value) {
+        coil.UpdateType((Coil.Type)value);
+        delayConfiguration.SetActive(coil.type != Coil.Type.normal);
+    }
+
+    public void SetCoilDelay(string valueStr) {
+        float value = 0f;
+        float.TryParse(valueStr, out value);
+        coil.UpdateDelay(Mathf.Abs(value));
+        delayInputField.text = coil.delay.ToString();
+    }
+
+    public void CloseWindow() {
+        Destroy(gameObject);
+    }
+
+    void Start() {
+        SelectedObjects.instance.ClearSelection();
+        EditorInput.instance.gameObject.SetActive(false);
+        ComponentListBar.instance.Disable();
+        Taskbar.instance.Disable();
+        SetupInitialValues();
+    }
+
+    void SetupInitialValues() {
+        typeDropdown.value = (int)coil.type;
+        delayInputField.text = coil.delay.ToString();
+        delayConfiguration.SetActive(coil.type != Coil.Type.normal);
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            CloseWindow();
+    }
+
+    void OnDestroy() {
+        EditorInput.instance.gameObject.SetActive(true);
+        ComponentListBar.instance.Enable();
+        Taskbar.instance.Enable();
+    }
+}
