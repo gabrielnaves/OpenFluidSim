8484084c4e82a42a3090772ddfe7ce3bc378707e
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-06-23 18:36:00 -0300

    refactoring componentlibrary.cs

--- Assets/Scripts/GUI/Component List/ComponentLibrary.cs
+++ Assets/Scripts/GUI/Component List/ComponentLibrary.cs
@@ -5,22 +5,17 @@ public class ComponentLibrary : MonoBehaviour {
 
     static public ComponentLibrary instance { get; private set; }
 
-    public string[] componentNames;
     public GameObject[] componentPrefabs;
-
     public Dictionary<string, GameObject> nameToPrefab { get; private set; }
 
     void Awake() {
         instance = (ComponentLibrary)Singleton.Setup(this, instance);
-        if (componentNames.Length != componentPrefabs.Length)
-            Debug.LogError("Component library configuration error");
-        else
-            BuildComponentTable();
+        BuildComponentTable();
     }
 
     void BuildComponentTable() {
         nameToPrefab = new Dictionary<string, GameObject>();
-        for (int i = 0; i < componentNames.Length; ++i)
-            nameToPrefab[componentNames[i]] = componentPrefabs[i];
+        foreach (var prefab in componentPrefabs)
+            nameToPrefab[prefab.name] = prefab;
     }
 }
