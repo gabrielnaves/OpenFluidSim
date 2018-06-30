77b6e52a0329130d26297b5341f73b82cf95520f
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-06-30 12:50:38 -0300

    Adding 5/3 valve

---
+++ Assets/Scripts/Simulation/Pneumatic Components/PneumaticValve_5_3_Simulation.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PneumaticValve_5_3_Simulation : PneumaticComponentSimulation {
+
+    public Transform[] componentsToAnimate;
+    public float shiftAmount;
+
+    public enum ValveState { left, middle, right }
+    [ViewOnly] public ValveState state = ValveState.middle;
+
+    Dictionary<ValveState, int[]> internalConnections = new Dictionary<ValveState, int[]> {
+        { ValveState.left, new int[]{ 3, 4, -1, 0, 1 } },
+        { ValveState.middle, new int[]{ -1, -1, -1, -1, -1 } },
+        { ValveState.right, new int[]{ 2, 3, 0, 1, -1 } },
+    };
+
+    Vector3[] originalPositions;
+    ComponentReferences componentReferences;
+    bool simulating;
+
+    void Awake() {
+        componentReferences = GetComponent<ComponentReferences>();
+    }
+
+    public override void Setup() {
+        state = ValveState.middle;
+        originalPositions = new Vector3[componentsToAnimate.Length];
+        for (int i = 0; i < componentsToAnimate.Length; ++i)
+            originalPositions[i] = componentsToAnimate[i].position;
+        simulating = true;
+    }
+
+    public override void Stop() {
+        state = ValveState.middle;
+        for (int i = 0; i < componentsToAnimate.Length; ++i)
+            componentsToAnimate[i].position = originalPositions[i];
+        originalPositions = null;
+        simulating = false;
+    }
+
+    public override void RespondToSignal(Connector sourceConnector, float signal) {
+        var connectorList = componentReferences.connectorList;
+        int sourceConnectorIndex = connectorList.IndexOf(sourceConnector);
+        int targetConnectorIndex = internalConnections[state][sourceConnectorIndex];
+        if (targetConnectorIndex != -1)
+            PneumaticSimulationEngine.instance.SpreadSignal(connectorList[targetConnectorIndex], signal);
+    }
+
+    void LateUpdate() {
+        if (simulating) {
+            PneumaticSolenoid leftSolenoid = componentReferences.solenoidList[0];
+            PneumaticSolenoid rightSolenoid = componentReferences.solenoidList[1];
+            if (leftSolenoid.active && !rightSolenoid.active) {
+                if (state != ValveState.left) {
+                    state = ValveState.left;
+                    for (int i = 0; i < componentsToAnimate.Length; ++i)
+                        componentsToAnimate[i].position = originalPositions[i] + transform.right * shiftAmount;
+                }
+            }
+            else if (!leftSolenoid.active && rightSolenoid.active) {
+                if (state != ValveState.right) {
+                    state = ValveState.right;
+                    for (int i = 0; i < componentsToAnimate.Length; ++i)
+                        componentsToAnimate[i].position = originalPositions[i] - transform.right * shiftAmount;
+                }
+            }
+            else if (state != ValveState.middle) {
+                state = ValveState.middle;
+                for (int i = 0; i < componentsToAnimate.Length; ++i)
+                    componentsToAnimate[i].position = originalPositions[i];
+            }
+        }
+    }
+}
