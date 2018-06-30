cb3bfbb1cecdf646600e1594419880c7a75aa732
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-06-30 12:58:43 -0300

    Implemented a button for component deletion

---
+++ Assets/Scripts/GUI/Taskbar/DeleteComponentButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteComponentButton : TaskbarButton {
+
+    public void DeleteComponents() {
+        List<BaseComponent> componentsToDelete = SelectedObjects.instance.GetSelectedComponents();
+        List<Wire> wiresToDelete = SelectedObjects.instance.GetSelectedWires();
+        if (componentsToDelete.Count > 0 || wiresToDelete.Count > 0)
+            ActionStack.instance.PushAction(new DeleteObjectsAction(componentsToDelete, wiresToDelete));
+    }
+
+    protected override bool ShouldShowButton() {
+        return SelectedObjects.instance.HasObject();
+    }
+}
