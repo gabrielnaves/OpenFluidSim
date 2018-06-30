38a9a214d5e30fa8bd2ef0e8d1d0809eeea578d7
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-06-30 13:18:04 -0300

    Adding rotation buttons to taskbar

---
+++ Assets/Scripts/GUI/Taskbar/RotateComponentButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateComponentButton : TaskbarButton {
+
+    public void RotateComponents(bool clockwise) {
+        List<BaseComponent> componentsToRotate = SelectedObjects.instance.GetSelectedComponents();
+        if (componentsToRotate.Count > 0)
+            ActionStack.instance.PushAction(new RotateComponentAction(
+                componentsToRotate,
+                clockwise
+            ));
+    }
+
+    protected override bool ShouldShowButton() {
+        if (!SelectedObjects.instance.HasObject())
+            return false;
+        return SelectedObjects.instance.GetSelectedComponents().Count > 0;
+    }
+}
